bfd1205792fc603ace67c6f387417841153d8e7c
amidone <tryguesswhodisis@gmail.com>
2017-06-21 18:56:45 +1000

    first commit

---
+++ mysite/personal/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request): 
+	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['if you would like to contact me, please email me,', 'konanastasiou1@gmail.com']})
